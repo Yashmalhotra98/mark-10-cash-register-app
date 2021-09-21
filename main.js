5137a5dea89ff7c365a3d52b7db0e06d38d4a17f
Yashmalhotra98 <Blackhacker1998@Gmail.com>
2021-09-22 01:40:27 +0530

    added html, css & js files

--- main.js
+++ main.js
@@ -1,33 +1,49 @@
-var billAmount = document.querySelector('#bill-amount');
-var cashGiven = document.querySelector('#cash-given');
-var checkButton = document.querySelector('#check-button');
-var message = document.querySelector('#error-message');
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+const remainAmt = document.querySelector("#remaining-amt");
 
-// console.log(billAmount, cashGiven);
+// checkButton.addEventListener('click', function getValues() {
+//     console.log(billAmount, cashGiven);
+// })
+const availableNotes = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
 
-checkButton.addEventListener('click', checkClickHandler);
-
-var billAmt = billAmount.Value;
-var cashGvn = cashGiven.Value;
-
-function validateBillAmount(billAmt) {
-    if (billAmt > 0) {
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    hideMessage();
+    if (Number(billAmount.value) > 0) {
+        if (Number(cashGiven.value) >= Number(billAmount.value)) {
+            const amountToBeReturned = Number(cashGiven.value) - Number(billAmount.value);
+            remainAmt.innerText = amountToBeReturned;
+            calculateChange(amountToBeReturned);
+        } else {
+            remainAmt.innerText = "";
+            showMessage("Please Enter Appropirate Cash!");
+        }
 
     } else {
-        message.innerText = 'The bill amount should be greater than 0';
+        showMessage("Invalid Bill Amount");
+    }
+});
+
+function calculateChange(amountToBeReturned) {
+    //for getting all the available notes
+    for (let i = 0; i < availableNotes.length; i++) {
+        //no of notes needed for the denomination
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+        //amount left after calculating the number of notes needed
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        //updating the no of notes in the table    
+        noOfNotes[i].innerText = numberOfNotes;
     }
 }
 
-function checkCashAmount(cashGvn, billAmt) {
-    if (cashGvn > billAmt) {
-
-    } else {
-        message.innerText = 'The Cash Given must be greater than the Bill Amount';
-    }
+function hideMessage() {
+    message.style.display = "none";
 }
 
-function checkClickHandler() {
-    validateBillAmount(billAmt);
-    checkCashAmount(cashGvn, billAmt);
-
+function showMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg;
 }
